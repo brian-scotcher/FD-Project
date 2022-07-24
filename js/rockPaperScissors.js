d5d2c53be42692af166ed397536c708bb7590f93
brian-scotcher <60779771+brian-scotcher@users.noreply.github.com>
2022-07-24 22:05:30 +0300

    js2 assignment rewritten

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,143 +1,123 @@
+//VARIABLES
 
-//start rock paper scissors game
-function startGame(){
-    //hides start button and instructions
-    document.getElementById("rpsInstructions").style.display = "none";
-    document.getElementById("startButton").style.display = "none";
+const optionsArr = ["rock", "paper", "scissors"];
 
-    //create divs
-    //div for game
-    const rpsBtnArea = document.createElement('div');
+let playerScore = 0;
+let aiScore = 0;
 
-    //div and p for results
-    const resultArea=document.createElement('div');
+let aiChoice = computerPlay(optionsArr);
+
+//CREATE ELEMENTS
+//score bar and content
+const scorebar = document.createElement('div');
+scorebar.setAttribute("id", "scorebar");
+scorebar.style.display="flex";
+scorebar.style.flexDirection="row";
+scorebar.style.justifyContent="space-between"
+
+const playerScoreDisplay = document.createElement('p');
+const aiScoreDisplay = document.createElement('p');
+const playerScoreText = document.createTextNode("Player Score: " + playerScore);
+const aiScoreText = document.createTextNode("Computer Score: " + aiScore);
+playerScoreDisplay.style.width="fit-content";
+aiScoreDisplay.style.width="fit-content";
+
+
+
+//game area and content
+const gameArea = document.createElement('div');
+gameArea.setAttribute("id", "gameArea")
+
+
+//rock button
+const rockBtn = document.createElement("button");
+rockBtn.setAttribute('id', 'rockButton');
+rockBtn.textContent = 'ROCK';
+
+//paper button
+paperBtn = document.createElement("button");
+paperBtn.setAttribute('id', 'paperButton');
+paperBtn.textContent = 'PAPER';
+
+//Scissors button
+const scissorsBtn = document.createElement("button");
+scissorsBtn.setAttribute('id', 'scissorsButton');
+scissorsBtn.textContent = 'SCISSORS';
+
+
+//FUNCTIONS
+function gameStart() {
+    gameSetup();
+
+
+}
+
+function gameSetup() {//sets up the start of the game
+    //Get Elements
+    const rockPaperScissors = document.getElementById("rockPaperScissors");
+    const rpsTitle = document.getElementById("rpsTitle");
+    const instructions = document.getElementById("instructions");
+    const startBtn = document.getElementById("startBtn");
+
     
-    resultArea.setAttribute("id", "resultArea")
-    resultArea.style.flex="1";
-    
-    const userChoiceText=document.createElement('p');
-    const aiChoiceText=document.createElement('p');
-    const result=document.createElement('p');
 
-    //create buttons
-    //rock
-    const rockBtn = document.createElement("button");
-    rockBtn.setAttribute('id', 'rockButton');
-    rockBtn.textContent = 'ROCK';
-    
-    //paper
-    const paperBtn = document.createElement("button");
-    paperBtn.setAttribute('id', 'paperButton');
-    paperBtn.textContent = 'PAPER';
+    //styling
+    instructions.style.display= "none";
+    startBtn.style.display= "none";
 
-    //Scissors
-    const scissorsBtn = document.createElement("button");
-    scissorsBtn.setAttribute('id', 'scissorsButton');
-    scissorsBtn.textContent = 'SCISSORS';
-    
-    //add buttons to DOM
-    document.getElementById("rockPaperScissors").appendChild(rpsBtnArea);
-    rpsBtnArea.appendChild(rockBtn);
-    rpsBtnArea.appendChild(paperBtn);
-    rpsBtnArea.appendChild(scissorsBtn);
-    
-    //Array for options
-    const optionsArr=["rock", "paper", "scissors"];
+    //append scorebar and contents
+    rockPaperScissors.appendChild(scorebar);
+    scorebar.appendChild(playerScoreDisplay);
+    scorebar.appendChild(aiScoreDisplay);
+    playerScoreDisplay.appendChild(playerScoreText);
+    aiScoreDisplay.appendChild(aiScoreText);
+    rockPaperScissors.appendChild(gameArea);
 
-    //FUNCTIONS
-    //computer chooses
-    function computerPlay(computerOptions){//Computer returns random value from array
-        return computerOptions[Math.floor(Math.random()*computerOptions.length)];
+}
+
+//Evaluation of player vs computer
+function evaluation(user, ai){
+    if(user===ai){
         
-    }
-
-    let aiChoice = computerPlay(optionsArr);
-    
-    //player choice
-    function rockClicked() {
-        if (rockClicked=true){
-            declareWinner(optionsArr[0]);
-        }
-    }
-
-    function paperClicked() {
-        if (paperClicked=true){
-            declareWinner(optionsArr[1]);
-        }
-    }
-
-    function scissorsClicked() {
-        if (scissorsClicked=true){
-            declareWinner(optionsArr[2]);
-        }
-    }
-
-    //Evaluation of player vs computer
-    function evaluation(user, ai){
-        if(user===ai){
-            
-            return "You both chose the same, It's a draw!";
-            
-        }else if(user === "rock" && ai=== "scissors"){
-            
-            return "Rock blunts scissors. You win!";
-
-        }else if(user ==="paper" && ai ==="scissors"){
-
-            return "Scissors cut paper. You lose!"
-
-
-        }else if(user ==="paper" && ai ==="rock"){
-            
-            return "Paper wraps rock. You win!";
-
-        }else if(user ==="scissors" && ai ==="rock"){
-            
-            return "Rock blunts scissors. You lose!";
-
-        }else if(user ==="scissors" && ai ==="paper"){
-            
-            return "Scissors cut paper. You win!";
-
-        }else if(user ==="rock" && ai ==="paper"){
-            
-            return "Paper wraps rock. You lose!";
-
-        }
-    }
-
-    //declares winner and styles text
-    function declareWinner(userChoice){
-        /*console.log("You chose " + userChoice)
-        console.log("Computer chose " + aiChoice);
-        console.log(evaluation(userChoice, aiChoice));*/
-        document.getElementById("rockPaperScissors").appendChild(resultArea);
-
-
-        function style_P_Tags(pElements){
-            pElements.style.fontFamily="sans-serif";
-            pElements.style.color="rgb(187, 255, 148)";
-        }
-
-        style_P_Tags(result);
-        style_P_Tags(userChoiceText);
-        style_P_Tags(aiChoiceText);
+        return "You both chose the same, It's a draw!";
         
-        resultArea.appendChild(userChoiceText);
-        resultArea.appendChild(aiChoiceText);
-        resultArea.appendChild(result);
+    }else if(user === "rock" && ai=== "scissors"){
+        
+        return "Rock blunts scissors. You win!";
 
-        userChoiceText.textContent= "You chose " + userChoice;
-        aiChoiceText.textContent= "Computer chose "+aiChoice;
-        result.textContent = evaluation(userChoice, aiChoice);
+    }else if(user ==="paper" && ai ==="scissors"){
+
+        return "Scissors cut paper. You lose!"
+
+
+    }else if(user ==="paper" && ai ==="rock"){
+        
+        return "Paper wraps rock. You win!";
+
+    }else if(user ==="scissors" && ai ==="rock"){
+        
+        return "Rock blunts scissors. You lose!";
+
+    }else if(user ==="scissors" && ai ==="paper"){
+        
+        return "Scissors cut paper. You win!";
+
+    }else if(user ==="rock" && ai ==="paper"){
+        
+        return "Paper wraps rock. You lose!";
+
     }
+}
 
-    //event listeners
-    rockBtn.addEventListener("click", rockClicked);
-    paperBtn.addEventListener("click", paperClicked);
-    scissorsBtn.addEventListener("click", scissorsClicked);
-
-    
-    
+  //computer chooses
+  function computerPlay(computerOptions){//Computer returns random value from array
+    return computerOptions[Math.floor(Math.random()*computerOptions.length)];
     
 }
+
+
+//EVENT LISTENERS
+
+rockBtn.addEventListener("click", rockClicked);
+paperBtn.addEventListener("click", paperClicked);
+scissorsBtn.addEventListener("click", scissorsClicked);
